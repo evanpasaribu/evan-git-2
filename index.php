e8a4804bd293e2cd1e419fc28d5a799c576a16ba
Evan Pasaribu <evanpasaribu2000@gmail.com>
2024-01-25 02:51:58 +0700

    menambahkan ul di index.php

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, qui earum. Non facere sapiente in quae ullam totam debitis sit, praesentium, cum, quis dolorem aliquid omnis ut iste ipsa soluta.</p>
+    <ul>satu</ul>
+    <ul>dua</ul>
 </body>
 </html>
